ba17bb45615f519313228a93fc54413f4617b9c0
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-05-05 01:28:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,9 @@ function shortcut(s1, s2) {
   // Extract the initial letters of both strings
   const initial1 = s1.charAt(0);
   const initial2 = s2.charAt(0);
-
+if (initial1 ==="" || initial2 === "") {
+	return "";
+}
   // Return the concatenation of initial letters
   return initial1 + initial2;
 }
